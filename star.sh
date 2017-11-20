7c9a5d4d2f99c84735a06f88983771e835f148d8
AbhikChowdhury6 <abhik.ch6@gmail.com>
2017-11-20 12:30:58 -0700

    added Nvidia stuff

--- star.sh
+++ star.sh
@@ -1,3 +1,27 @@
 #!/bin/bash
 
-sudo apt install htop
+nvidiaVersion = "384"
+sudo apt install -y libxss1 libappindicator1 libindicator7
+sudo apt install -y htop tmux
+
+sudo apt-get install -y python3-pip python3-dev python-virtualenv
+
+#chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome*.deb
+sudo apt-get install -f
+rm google-chrome*
+
+#Nvidia stuff
+sudo add-apt-repository -y ppa:graphics-drivers/ppa
+sudo apt update
+sudo apt install -y nvidia$nvidiaVersion
+
+#tensorflow
+google-chrome https://developer.nvidia.com/rdp/form/cudnn-download-survey
+pip3 install --upgrade tensorflow-gpu
+
+#atom
+wget https://atom.io/download/deb
+sudo dpkg -i atom*
+rm atom*
