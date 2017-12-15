99ef527c047e166e2e025db4e94902c9ad1dbdb2
AbhikChowdhury6 <abhik.ch6@gmail.com>
2017-12-15 15:53:11 -0700

    added node

--- star.sh
+++ star.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 nvidiaVersion = "384"
+
 sudo apt install -y libxss1 libappindicator1 libindicator7
 sudo apt install -y htop tmux
 
 sudo apt-get install -y python3-pip python3-dev python-virtualenv
+sudo apt-get install -y python-pip python-dev
+
 
 #chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
@@ -17,11 +20,23 @@ sudo add-apt-repository -y ppa:graphics-drivers/ppa
 sudo apt update
 sudo apt install -y nvidia$nvidiaVersion
 
+#amd stuff
+
+sudo apt install radeontop
+
+
 #tensorflow
+
 google-chrome https://developer.nvidia.com/rdp/form/cudnn-download-survey
-pip3 install --upgrade tensorflow-gpu
+
+pip install --upgrade tensorflow-gpu
+pip install -U matplotlib
 
 #atom
 wget https://atom.io/download/deb
 sudo dpkg -i atom*
 rm atom*
+
+#node
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt-get install -y nodejs
